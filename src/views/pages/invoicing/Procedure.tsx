7df9300b3d06ae8098402c99a5aa5328b6c1aa07
Matheus Robert de Melo <matheusroberttjmelo@gmail.com>
2024-10-28 15:02:15 -0300

    chore: initial structure

---
+++ src/views/pages/invoicing/Procedure.tsx
@@ -0,0 +1,14 @@
+// material-ui
+import Typography from '@mui/material/Typography';
+
+// project imports
+import MainCard from 'ui-component/cards/MainCard';
+
+// ==============================|| Procedure PAGE ||============================== //
+
+const Procedure = () => (
+    <MainCard title="Cadastro de Procedimentos">
+    </MainCard>
+);
+
+export default Procedure;
