306ad91be0fb85fb1bbdf61f2861693fc45520ce
kevenMelo <keventjmelo@gmail.com>
2024-11-01 19:17:35 -0300

    feat: adicionar formulário de cadastro de procedimentos com validação usando zod e integração com MUI

--- src/views/pages/invoicing/Procedure.tsx
+++ src/views/pages/invoicing/Procedure.tsx
@@ -1,7 +1,3 @@
-// material-ui
-// import Typography from '@mui/material/Typography';
-
-// project imports
 import * as React from 'react';
 import MainCard from 'ui-component/cards/MainCard';
 import TextField from '@mui/material/TextField';
@@ -11,62 +7,24 @@ import { Box } from '@mui/material';
 import InputAdornment from '@mui/material/InputAdornment';
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import Fab from '@mui/material/Fab';
-import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
 import Switch from '@mui/material/Switch';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-// ==============================|| Procedure PAGE ||============================== //
+import ProcedureForm from './ProcedureForm';
+// Mock data
 
-const columns: GridColDef<(typeof rows)[number]>[] = [
-    {
-        field: 'id',
-        headerName: 'ID',
-        width: 170
-    },
-    {
-        field: 'Descrição do Procedimento',
-        headerName: 'Descrição do Procedimento',
-        width: 170
-    },
-    {
-        field: 'Código CBHPM',
-        headerName: 'Código CBHPM',
-        width: 170
-    },
-    {
-        field: 'Instituição',
-        headerName: 'Instituição',
-        width: 170
-    },
-    {
-        field: 'Modalidade',
-        headerName: 'Modalidade',
-        width: 170
-    },
-    {
-        field: 'Editar',
-        headerName: 'Editar',
-        width: 170,
-        renderCell: () => <Edit color="primary" />
-    },
-    {
-        field: 'Inativo/Ativo',
-        headerName: 'Inativo/Ativo',
-        width: 170,
-        renderCell: () => <Switch />
-    }
+// Columns for DataGrid
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 170 },
+    { field: 'Descrição do Procedimento', headerName: 'Descrição do Procedimento', width: 170 },
+    { field: 'Código CBHPM', headerName: 'Código CBHPM', width: 170 },
+    { field: 'Instituição', headerName: 'Instituição', width: 170 },
+    { field: 'Modalidade', headerName: 'Modalidade', width: 170 },
+    { field: 'Editar', headerName: 'Editar', width: 170, renderCell: () => <Edit color="primary" /> },
+    { field: 'Inativo/Ativo', headerName: 'Inativo/Ativo', width: 170, renderCell: () => <Switch /> }
 ];
 
-// Mocked Row. TODO: Implementar a chamada da API para popular a tabela
+// Mocked rows
 const rows = [
     {
         id: 1,
@@ -112,15 +70,6 @@ const rows = [
 
 const Procedure = () => {
     const [open, setOpen] = React.useState(false);
-    const [institute, setInstitute] = React.useState<string[]>([]);
-    const [modality, setModality] = React.useState<string[]>([]);
-    const handleChange = (event: SelectChangeEvent) => {
-        setInstitute(typeof event.target.value === 'string' ? event.target.value.split(',') : event.target.value);
-    };
-
-    const handleChangeModality = (event: SelectChangeEvent) => {
-        setModality(typeof event.target.value === 'string' ? event.target.value.split(',') : event.target.value);
-    };
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -130,13 +79,11 @@ const Procedure = () => {
         setOpen(false);
     };
 
-    const mockInstitutes = ['Teste1', 'Teste2', 'Teste3'];
-    const mockModalities = ['Teste1', 'Teste2', 'Teste3'];
     return (
         <>
             <MainCard title="Cadastro de Procedimentos">
                 <Box display="flex" justifyContent="space-between">
-                    <TextField /// TODO: Pedi orientação para implementar o layout do label "search" inside do textfield
+                    <TextField
                         id="input-with-icon-textfield"
                         variant="outlined"
                         label="Search"
@@ -148,7 +95,6 @@ const Procedure = () => {
                                     </Box>
                                 </InputAdornment>
                             )
-                            // <Search sx={{ color: 'action.active', mr: 0, my: 0.5 }} />
                         }}
                     />
                     <Fab size="small" color="primary" aria-label="add" onClick={handleClickOpen}>
@@ -161,129 +107,17 @@ const Procedure = () => {
                     width="100%"
                     height="80vh"
                     sx={{
-                        '& .MuiDataGrid-root': {
-                            border: 'none'
-                        },
-                        '& .MuiDataGrid-cell': {
-                            borderBottom: 'none',
-                            fontSize: '10px'
-                        },
-                        '& .name-column--cell': {},
-                        '& .MuiDataGrid-columnHeaders': {
-                            borderBottom: 'none',
-                            fontSize: '12px'
-                        },
-                        '& .MuiDataGrid-virtualScroller': {},
-                        '& .MuiDataGrid-footerContainer': {
-                            borderTop: 'none'
-                        },
-                        '& .MuiCheckbox-root': {},
-                        '& .MuiDataGrid-toolbarContainer .MuiButton-text': {}
+                        '& .MuiDataGrid-root': { border: 'none' },
+                        '& .MuiDataGrid-cell': { borderBottom: 'none', fontSize: '10px' },
+                        '& .MuiDataGrid-columnHeaders': { borderBottom: 'none', fontSize: '12px' },
+                        '& .MuiDataGrid-footerContainer': { borderTop: 'none' }
                     }}
                 >
                     <DataGrid rows={rows} columns={columns} />
                 </Box>
             </MainCard>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-                maxWidth={'md'}
-                fullWidth={true}
-                PaperProps={{
-                    component: 'form',
-                    onSubmit: (event: React.FormEvent<HTMLFormElement>) => {
-                        event.preventDefault();
-                        const formData = new FormData(event.currentTarget);
-                        const formJson = Object.fromEntries((formData as any).entries());
-                        const email = formJson.email;
-                        console.log(email);
-                        handleClose();
-                    },
-                    sx: {
-                        zIndex: 1200 // Ajuste o z-index do Dialog
-                    }
-                }}
-            >
-                <DialogTitle>Procedimentos</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Cadastro de Procedimento: Insira todas as informações necessárias para o procedimento, incluindo descrição, código,
-                        instituição e modalidade. Verifique se os dados estão corretos antes de salvar. Confirme se deseja cadastrar este
-                        procedimento.
-                    </DialogContentText>
-                    <Box display="flex" justifyContent="space-between" gap={2}>
-                        <Box width="180%">
-                            <TextField
-                                autoFocus
-                                required
-                                label="Descrição do Procedimento"
-                                name="description"
-                                variant="outlined"
-                                fullWidth
-                            />
-                        </Box>
-                        <TextField required label="Código CBHPM" name="code" variant="outlined" fullWidth />
-                    </Box>
-                    <Box height={10} />
-                    <Box display="flex" justifyContent="space-between">
-                        <Box width="60vh">
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-multiple-name-label" style={{ color: 'black' }}>
-                                    Instituto
-                                </InputLabel>
-                                <Select
-                                    labelId="demo-multiple-name-label"
-                                    id="demo-multiple-name"
-                                    multiple
-                                    // defaultValue="Teste1"
-                                    value={institute}
-                                    label="Instituição"
-                                    onChange={handleChange}
-                                    fullWidth
-                                >
-                                    {mockInstitutes.map((mockInstitute) => (
-                                        <MenuItem key={mockInstitute} value={mockInstitute}>
-                                            {mockInstitute}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </Box>
-                        <Box width="52vh">
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-multiple-name-label" style={{ color: 'black' }}>
-                                    Modalidade
-                                </InputLabel>
-                                <Select
-                                    labelId="demo-multiple-name-label"
-                                    id="demo-simple-select"
-                                    multiple
-                                    value={modality}
-                                    onChange={handleChangeModality}
-                                    fullWidth
-                                >
-                                    {mockModalities.map((mockModality) => (
-                                        <MenuItem key={mockModality} value={mockModality}>
-                                            {mockModality}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                        </Box>
-                    </Box>
-                    <Box height={50} />
-                </DialogContent>
-                <DialogActions>
-                    <Button variant="outlined" onClick={handleClose} color="primary">
-                        Fechar
-                    </Button>
-                    <Box width={10} />
-                    <Button variant="contained" type="submit" color="primary">
-                        Salvar
-                    </Button>
-                    <Box width={10} />
-                </DialogActions>
-            </Dialog>
+
+            <ProcedureForm open={open} handleClose={handleClose} />
         </>
     );
 };
---
+++ src/views/pages/invoicing/ProcedureForm.tsx
@@ -0,0 +1,177 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import { z } from 'zod';
+
+// Mock data
+const mockInstitutes = ['Teste1', 'Teste2', 'Teste3'];
+const mockModalities = ['Teste1', 'Teste2', 'Teste3'];
+
+// Validation schema using zod
+const procedureSchema = z.object({
+    description: z.string().nonempty('Descrição é obrigatória'),
+    code: z.string().nonempty('Código CBHPM é obrigatório'),
+    institute: z.string().nonempty('Instituto é obrigatório'),
+    modality: z.string().nonempty('Modalidade é obrigatória')
+});
+
+type ProcedureFormProps = {
+    open: boolean;
+    handleClose: () => void;
+};
+
+const ProcedureForm: React.FC<ProcedureFormProps> = ({ open, handleClose }) => {
+    const [description, setDescription] = React.useState('');
+    const [code, setCode] = React.useState('');
+    const [institute, setInstitute] = React.useState<string[]>([]);
+    const [modality, setModality] = React.useState<string[]>([]);
+    const [errors, setErrors] = React.useState({
+        description: '',
+        code: '',
+        institute: '',
+        modality: ''
+    });
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = {
+            description,
+            code,
+            institute,
+            modality
+        };
+
+        const result = procedureSchema.safeParse(formData);
+
+        if (!result.success) {
+            const newErrors = result.error.format();
+            setErrors({
+                description: newErrors.description?._errors[0] || '',
+                code: newErrors.code?._errors[0] || '',
+                institute: newErrors.institute?._errors[0] || '',
+                modality: newErrors.modality?._errors[0] || ''
+            });
+        } else {
+            console.log(formData);
+            handleClose();
+        }
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth>
+            <form onSubmit={handleSubmit}>
+                <DialogTitle>Procedimentos</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Cadastro de Procedimento: Insira todas as informações necessárias para o procedimento, incluindo descrição, código,
+                        instituição e modalidade. Verifique se os dados estão corretos antes de salvar. Confirme se deseja cadastrar este
+                        procedimento.
+                    </DialogContentText>
+                    <Box height={50} />
+                    <Box display="flex" justifyContent="space-between" gap={2}>
+                        <Box width="180%">
+                            <TextField
+                                autoFocus
+                                required
+                                label="Descrição do Procedimento"
+                                name="description"
+                                variant="outlined"
+                                fullWidth
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                                error={Boolean(errors.description)}
+                                helperText={errors.description}
+                            />
+                        </Box>
+                        <TextField
+                            required
+                            label="Código CBHPM"
+                            name="code"
+                            variant="outlined"
+                            fullWidth
+                            value={code}
+                            onChange={(e) => setCode(e.target.value)}
+                            error={Boolean(errors.code)}
+                            helperText={errors.code}
+                        />
+                    </Box>
+                    <Box height={10} />
+                    <Box display="flex" justifyContent="space-between">
+                        <Box width="60vh">
+                            <FormControl fullWidth error={Boolean(errors.institute)}>
+                                <InputLabel id="institute-label" style={{ color: 'black' }}>
+                                    Instituto
+                                </InputLabel>
+                                <Select
+                                    labelId="institute-label"
+                                    id="institute-select"
+                                    multiple
+                                    value={institute}
+                                    label="Instituição"
+                                    onChange={(e) => {
+                                        setInstitute(e.target.value as string[]);
+                                    }}
+                                    fullWidth
+                                >
+                                    {mockInstitutes.map((mockInstitute) => (
+                                        <MenuItem key={mockInstitute} value={mockInstitute}>
+                                            {mockInstitute}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                {errors.institute && <Box color="error.main">{errors.institute}</Box>}
+                            </FormControl>
+                        </Box>
+                        <Box width="52vh">
+                            <FormControl fullWidth error={Boolean(errors.modality)}>
+                                <InputLabel id="modality-label" style={{ color: 'black' }}>
+                                    Modalidade
+                                </InputLabel>
+                                <Select
+                                    labelId="modality-label"
+                                    id="modality-select"
+                                    multiple
+                                    value={modality}
+                                    onChange={(e) => {
+                                        setModality(e.target.value as string[]);
+                                    }}
+                                    fullWidth
+                                >
+                                    {mockModalities.map((mockModality) => (
+                                        <MenuItem key={mockModality} value={mockModality}>
+                                            {mockModality}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                                {errors.modality && <Box color="error.main">{errors.modality}</Box>}
+                            </FormControl>
+                        </Box>
+                    </Box>
+                    <Box height={50} />
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="outlined" onClick={handleClose} color="primary">
+                        Fechar
+                    </Button>
+                    <Box width={10} />
+                    <Button variant="contained" type="submit" color="primary">
+                        Salvar
+                    </Button>
+                    <Box width={10} />
+                </DialogActions>
+            </form>
+        </Dialog>
+    );
+};
+
+export default ProcedureForm;
